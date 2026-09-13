ced019bb6d5f3689c588f369ab798a1e99f6d3ac
defnae <thinkimlost@proton.me>
2026-09-13 05:02:16 +0000

    feat: working editor

---
+++ source/headers/def.h
@@ -0,0 +1,14 @@
+// source/headers/def.h
+
+#ifndef DEF_H
+#define DEF_H
+
+#define NULL ((void*) 0)
+
+#define offsetof __builtin_offsetof
+
+typedef __SIZE_TYPE__ size_t;
+
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+
+#endif /* DEF_H */
